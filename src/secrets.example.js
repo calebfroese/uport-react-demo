1e087ebbc41f319e50a76ee0021667ab844b9842
Caleb Froese <calebfroese@Calebs-iMac.local>
2018-07-26 23:19:07 +0930

    Move sign key into secrets

---
+++ src/secrets.example.js
@@ -0,0 +1,2 @@
+// Copy this file over as `secrets.js`
+export const SIGN_KEY = ''; // your uPort app sign key
--- src/util/connectors.js
+++ src/util/connectors.js
@@ -1,4 +1,31 @@
-import { Connect } from 'uport-connect'
+import {
+    Connect,
+    SimpleSigner
+} from 'uport-connect'
+import {
+    SIGN_KEY
+} from '../secrets'
 
-export let uport = new Connect('TruffleBox')
-export const web3 = uport.getWeb3()
+export const uport = new Connect('Caleb Froese\'s React App', {
+    clientId: '2ohgvXAEpnFWxRxbxFySPr8Ne7E19eS6XBd',
+    network: 'rinkeby',
+    signer: SimpleSigner(SIGN_KEY)
+})
+
+// Request credentials to login
+// uport.requestCredentials({
+//   requested: ['name', 'phone', 'country'],
+//   notifications: true // We want this if we want to recieve credentials
+// })
+// .then((credentials) => {
+//   console.log(credentials);
+// })
+
+// Attest specific credentials
+// uport.attestCredentials({
+//   sub: THE_RECEIVING_UPORT_ADDRESS,
+//   claim: {
+//     CREDENTIAL_NAME: CREDENTIAL_VALUE
+//   },
+//   exp: new Date().getTime() + 30 * 24 * 60 * 60 * 1000, // 30 days from now
+// })
